ce6c01f363d8a5f0bed19f5d81d0d9ff0e1e7507
SaldivarAdiii <irishbanganan31@gmail.com>
2024-03-12 08:10:02 +0800

    Initial commit

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle  } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonCard>
+
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+  );
+}
+
+export default Tab1;
