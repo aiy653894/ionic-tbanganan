ae49c7d0e75c2d784690e10e04a88bb38f291345
SaldivarAdiii <irishbanganan31@gmail.com>
2024-03-12 09:01:16 +0800

    ädded alert function

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
-  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
-
-const Tab1: React.FC = () => {
-  return (
-    <IonCard>
-    <img alt="Silhouette of mountains" src="../src/assets/img/avatar.png" />
-    <IonCardHeader>
-      <IonCardTitle>Card Title</IonCardTitle>
-      <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-  </IonCard>
-
-    
-);
-}
-
-export default Tab1;
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton  } from '@ionic/react';
+  import ExploreContainer from '../components/ExploreContainer';
+  import './Tab1.css';
+  
+  const Tab1: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>profile</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">profile</IonTitle>
+            </IonToolbar>
+  
+            
+          </IonHeader>
+          <IonCard>
+        <img alt="Silhouette of mountains" src="../src/assets/img/avatar.PNG" />
+        <IonCardHeader>
+          <IonCardTitle>irish</IonCardTitle>
+          <IonCardSubtitle>student</IonCardSubtitle>
+        </IonCardHeader>
+  
+        <IonCardContent>Don't be trapped in someone else's dream</IonCardContent>
+        <IonButton id="present-alert" expand='full'>Click Me</IonButton>
+  
+  
+      </IonCard>
+        <IonAlert
+          header="Alert!"
+          trigger="present-alert"
+          buttons={[
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                console.log('Alert canceled');
+              },
+            },
+            {
+              text: 'OK',
+              role: 'confirm',
+              handler: () => {
+                console.log('Alert confirmed');
+              },
+            },
+          ]}
+          onDidDismiss={({ detail }) => console.log('Dismissed with role: ${detail.role}')}
+        ></IonAlert>
+      
+      
+  
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Tab1;
